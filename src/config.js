5b7b094b930b1d3545b0123e1a00a126faadeea6
sevebarr <seve.barrameda@gmail.com>
2020-03-20 14:09:57 +0800

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,12 +1,15 @@
 export default {
+  STRIPE_KEY: "pk_test_x3GKxwPlju7kWGhDiHNnC9EZ00XbVxdQQI",
+
   s3: {
     REGION: "us-east-2",
     BUCKET: "sevesfirstawsbucket"
   },
   apiGateway: {
-    REGION: " us-east-2",
+    REGION: "us-east-2",
     URL: "https://ybvz8azu1f.execute-api.us-east-2.amazonaws.com/prod"
   },
+
   cognito: {
     REGION: "us-east-2",
     USER_POOL_ID: "us-east-2_2NnHjMSJa",
